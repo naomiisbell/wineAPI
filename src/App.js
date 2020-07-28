eaf4e897048d47cca3b9fe80046ad32f60fe50b2
Naomi Isbell <naomiisbell@gmail.com>
2020-07-28 11:51:58 -0400

    working components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Names from "./components/Names"
+import Picture from "./components/Picture";
+import Grapes from "./components/Grapes"
+import Country from "./components/Country"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Names />
+      <Picture />
+      <Grapes />
+      <Country />
     </div>
-  );
+  )
 }
+
 
 export default App;
